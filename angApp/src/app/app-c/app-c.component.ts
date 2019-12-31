969102bcaed7bfbef417ef285656b1a735e016c9
AJ <aayush.jaiswal984@gmail.com>
2019-12-31 11:20:35 +0530

    For IF

--- angApp/src/app/app-c/app-c.component.ts
+++ angApp/src/app/app-c/app-c.component.ts
@@ -8,6 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class AppCComponent implements OnInit {
 
   constructor() { }
+  public names = [
+    { name: "Kamal"},
+    { name: "Mitchel"},
+    { name: "Yoon"},
+    { name: "Johnson"},
+    { name: "Jet Li"}
+  ];
+
+  employee =[
+    {empName: "John Corter", empID: "1001", empDOJ: "20 Nov 2019", empSal: "25 K"},
+    {empName: "Mickle", empID: "1002", empDOJ: "1 Dec 2019", empSal: "30 K"},
+  ];
+
+  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
 
   ngOnInit() {
   }
